2c538186c0425cddc1e8d27120f4663e7664d75d
Kelly Lynch <kellydlynch@gmail.com>
2017-01-19 10:46:51 -0700

    Changes to miniatures.js, restructuring

--- plugins/kd_miniatures.js
+++ plugins/kd_miniatures.js
@@ -2,131 +2,277 @@ let request = require('request');
 let cheerio = require('cheerio');
 let _ = require('lodash');
 let async = require('async');
+let store = require('../utils/store');
+let miniatures = store.createNameSpace('miniatures');
 
-let store = require('../utils/store')
+let cmds =
+  "help|" +
+  "all|" +
+  "summary|sum|summery|info|information|" +
+  "sculptor|sculpt|sculpter|" +
+  "artist|" +
+  "status|stat|" +
+  "firstsold|sold|first|" +
+  "gameplay|game|" +
+  "notes|note|" +
+  "lore|" +
+  "art|artwork|concept|conceptart|" +
+  "pictures|pic|picture|" +
+  "buildguide|build|guide|" +
+  "sets|set";
+let alreadyPulledData = false;
+let dataMinis;
+let dataArtists;
+let dataSculptors;
+let dataSets;
+let dataStatus;
+let latestUpdateMinis;
+let latestUpdateSculptors;
+let latestUpdateSets;
+let latestUpdateStatus;
 
-let miniatures = store.createNameSpace('miniatures')
+function pullDataMinis(cb) {
+  if (!dataMinis) {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-minis.json',
+      json: true
+    }, function(error, res, body) {
+      if (!error) {
+        dataMinis = body;
+        latestUpdateMinis = res.headers['last-modified'];
+        cb(body)
+      }
+    });
+  } else {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-minis.json',
+      method: 'HEAD'
+    }, function(error, res) {
+      if (latestUpdateMinis === res.headers['last-modified']) {
+        cb(dataMinis)
+      } else {
+        dataMinis = void 0;
+        pullDataMinis(cb)
+      }
+    });
+  }
+}
+function pullDataArtists(cb) {
+  if (!dataArtists) {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_artist.json',
+      json: true
+    }, function(error, res, body) {
+      if (!error) {
+        dataArtists = body;
+        latestUpdateArtists = res.headers['last-modified'];
+        cb(body)
+      }
+    });
+  } else {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_artist.json',
+      method: 'HEAD'
+    }, function(error, res) {
+      if (latestUpdateArtists === res.headers['last-modified']) {
+        cb(dataArtists)
+      } else {
+        dataArtists = void 0;
+        pullDataArtists(cb)
+      }
+    });
+  }
+}
+function pullDataSculptors(cb) {
+  if (!dataSculptors) {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_sculptor.json',
+      json: true
+    }, function(error, res, body) {
+      if (!error) {
+        dataSculptors = body;
+        latestUpdateSculptors = res.headers['last-modified'];
+        cb(body)
+      }
+    });
+  } else {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_sculptor.json',
+      method: 'HEAD'
+    }, function(error, res) {
+      if (latestUpdateSculptors === res.headers['last-modified']) {
+        cb(dataSculptors)
+      } else {
+        dataSculptors = void 0;
+        pullDataSculptors(cb)
+      }
+    });
+  }
+}
+function pullDataSets(cb) {
+  if (!dataSets) {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_sets.json',
+      json: true
+    }, function(error, res, body) {
+      if (!error) {
+        dataSets = body;
+        latestUpdateSets = res.headers['last-modified'];
+        cb(body)
+      }
+    });
+  } else {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_sets.json',
+      method: 'HEAD'
+    }, function(error, res) {
+      if (latestUpdateSets === res.headers['last-modified']) {
+        cb(dataSets)
+      } else {
+        dataSets = void 0;
+        pullDataSets(cb)
+      }
+    });
+  }
+}
+function pullDataStatus(cb) {
+  if (!dataStatus) {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_status.json',
+      json: true
+    }, function(error, res, body) {
+      if (!error) {
+        dataStatus = body;
+        latestUpdateStatus = res.headers['last-modified'];
+        cb(body)
+      }
+    });
+  } else {
+    request({
+      url: 'http://www.rubidiumhexafluorosilicate.com/kd/kd-min_status.json',
+      method: 'HEAD'
+    }, function(error, res) {
+      if (latestUpdateStatus === res.headers['last-modified']) {
+        cb(dataStatus)
+      } else {
+        dataStatus = void 0;
+        pullDataStatus(cb)
+      }
+    });
+  }
+}
+
+function isDataLoaded() {
+  if (!alreadyPulledData) {
+    let snork;
+    pullDataStatus(function(snork){});
+    pullDataArtists(function(snork){});
+    pullDataSculptors(function(snork){});
+    pullDataMinis(function(snork){});
+    pullDataSets(function(snork){});
+    alreadyPulledData = true;
+  }
+}
+
+//isDataLoaded();
 
 module.exports = function(match, say) {
-  let defaultResponse = "lore";
-  let dataMinis;
-  let latestUpdatedMinis;
 
-  function pullDataMinis(cb) {
-    if (!dataMinis) {
-      request({
-        url: 'http://www.rubidiumhexafluorosilicate.com/kd/kingdomdeath.json',
-        json: true
-      }, function(error, res, body) {
-        if (!error) {
-          dataMinis = body;
-          latestUpdatedMinis = res.headers['last-modified'];
-          cb(body)
-        }
-      });
-    } else {
-      request({
-        url: 'http://www.rubidiumhexafluorosilicate.com/kd/kingdomdeath.json',
-        method: 'HEAD'
-      }, function(error, res) {
-        if (latestUpdatedMinis === res.headers['last-modified']) {
-          cb(dataMinis)
-        } else {
-          dataMinis = void 0;
-          pullDataMinis(cb)
-        }
-      });
-    }
-  }
-  
-  function showMiniInfo(srch, command) {
-    let search = "";
-    let stype = defaultResponse; // what type of data are we hunting for?
-    let parser = command.split(' ');
-    parser[2] = parser.slice(2).join(' ');
+  let defaultResponse = "summary";
+  isDataLoaded();
+
+  function parseCommand(fullcmd) {
+    let parsed = {
+      cmd: '',
+      scmd: defaultResponse,
+      srch: '',
+      isId: false
+    };
+    fullcmd = (fullcmd.replace( /[\s\n\r]+/g, ' ' )).trim();
+    let parser = fullcmd.split(' ');
+    parsed.cmd = parser[0];
     if (parser.length <= 1) {
-      // only gave command ... ignore this stuff. :P
-      say("Whachoo tryin' to say, Willis?");
-      return false;
+      parsed.scmd = 'help';
+      return parsed;
     }
-    if (parser.length == 2) {
-      // okay, we have command and search item (default subcommand) OR command and subcommand (no search item)
-      // assume first a search item
-      search = parser[1].toLowerCase();
-      // check to see if it's a subcommand, and if so, move it there, and then blank the search term
-      if (search.match("lore|sum|summery|summary|sculpt|sculpter|sculptor|artist|note|notes|art|artwork|pic|picture|pictures")) {
-        stype = search;
-        search = "";
+    if (parser[1].match(cmds)) {
+      if (parser.length >= 3) {
+        parser[2] = parser.slice(2).join(' ');
       }
     } else {
-      // we got both command, subcommand and search item OR command and multiword search item (default subcommand)
-      // assume first subcommand/search
-      stype = parser[1].toLowerCase();
-      search = parser[2].toLowerCase();
-      // check to see if stype actually is a subcommand, else it must be multi-word search item \
-      if (!stype.match("lore|sum|summery|summary|sculpt|sculpter|sculptor|artist|note|notes|art|artwork|pic|picture|pictures")) {
-        search = stype + " " + search;
-        stype = defaultResponse;
+      if (parser.length >=2) {
+        parser[2] = parser.slice(1).join(' ');
+        parser[1] = defaultResponse;
       }
     }
+    parsed.scmd = parser[1].toLowerCase();
+    parsed.srch = parser[2].toLowerCase();
+    parsed.isId = /^\d+$/.test(parsed.srch);
+    return parsed
+  }
 
-    let isnum = /^\d+$/.test(search);
+
+  //let say = new Say(bot, config, from, to)
+  function showMiniInfo(subcmd, fullcmd) {
+    isDataLoaded();
+    parsed = parseCommand(fullcmd);
+
 
     pullDataMinis(function(body) {
       let entry;
-      if (isnum) {
-        entry = _.find(body.miniatures, function(a) {
-          return ~~a.id === ~~search
+      if (parsed.isId) {
+        entry = _.find(dataMinis.minis, function (a) {
+          return ~~a.id === ~~parsed.srch
         })
       } else {
-        entry = _.find(body.miniatures, function(a) {
-          return _.lowerCase(a.min_name) === _.lowerCase(search)
+        entry = _.find(dataMinis.minis, function (a) {
+          return _.lowerCase(a.min_name) === parsed.srch
         })
       }
       if (entry) {
-        switch (stype) {
+        switch (parsed.scmd) {
           case "sum":
           case "summ":
           case "summery":
           case "summary":
-            say(entry.min_name + "\n  Summary:  " + entry.min_lore);
+            say(entry.min_name + "\n  Summary:  " + entry.min_summary);
             miniatures.tempMatch('more', () => {
-              showMiniInfo(undefined, parser[0] + ' lore ' + search)
-            })
+              showMiniInfo(undefined, parsed.cmd + ' lore ' + parsed.srch)
+            });
             break;
           case "lore":
             say(entry.min_name + "\n  Lore:  " + entry.min_lore);
             miniatures.tempMatch('more', () => {
-              showMiniInfo(undefined, parser[0] + ' sculptor ' + search)
-            })
+              showMiniInfo(undefined, parsed.cmd + ' sculptor ' + parsed.srch)
+            });
             break;
           case "sculpt":
           case "sculpter":
           case "sculptor":
             say(entry.min_name + "\n  Sculptor:  " + entry.min_sculptor);
             miniatures.tempMatch('more', () => {
-              showMiniInfo(undefined, parser[0] + ' artist ' + search)
-            })
+              showMiniInfo(undefined, parsed.cmd + ' artist ' + parsed.srch)
+            });
             break;
           case "artist":
             say(entry.min_name + "\n  Artist:  " + entry.min_artist);
             miniatures.tempMatch('more', () => {
-              showMiniInfo(undefined, parser[0] + ' notes ' + search)
-            })
+              showMiniInfo(undefined, parsed.cmd + ' notes ' + parsed.srch)
+            });
             break;
           case "note":
           case "notes":
             say(entry.min_name + "\n  Notes:  " + entry.min_notes);
             miniatures.tempMatch('more', () => {
-              showMiniInfo(undefined, parser[0] + ' art ' + search)
-            })
+              showMiniInfo(undefined, parsed.cmd + ' art ' + parsed.srch)
+            });
             break;
           case "art":
           case "artwork":
             say(entry.min_name + "\n  Artwork:  " + entry.min_artwork);
             miniatures.tempMatch('more', () => {
-              showMiniInfo(undefined, parser[0] + ' pic ' + search)
-            })
+              showMiniInfo(undefined, parsed.cmd + ' pic ' + parsed.srch)
+            });
             break;
           case "pic":
           case "picture":
@@ -137,18 +283,23 @@ module.exports = function(match, say) {
             say(entry.min_name + "\n  Yeah, it exists. What of it? :p");
         }
       } else {
-        if (isnum) {
-          say("There is no miniature with index #" + search + " was not found.");
+        if (parsed.isId) {
+          say("There is no miniature with index #" + parsed.srch + " was not found.");
         } else {
-          say("The miniature '" + search + "' was not found.");
+          say("The miniature '" + parsed.srch + "' was not found.");
         }
-
       }
-    });
+    })
   }
 
-  match(['miniinfo', 'mini'], showMiniInfo)
+      //}
 
+
+
+
+  match(['miniinfo', 'mini', 'min'], showMiniInfo);
+
+  /*
   function reduceMinis(entries) {
     return _.reduce(entries, function(str, entry) {
       if (entry.id === _.last(entries).id) {
@@ -180,5 +331,5 @@ module.exports = function(match, say) {
 
     });
   })
-
-}
+  */
+};
